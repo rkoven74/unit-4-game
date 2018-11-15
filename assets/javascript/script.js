da0f447ac0389be54ed6d5ea1458cd583ac3e4e2
Roxanne Koven <44323143+rkoven74@users.noreply.github.com>
2018-11-14 20:08:09 -0700

    Add files via upload

---
+++ assets/javascript/script.js
@@ -0,0 +1,9 @@
+
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"
+
+
+create an array of crystal image links
+
+loop through the array of image links for each one;
+create a new "img" Element set src equal to crystal image link
+set random value (points value)
